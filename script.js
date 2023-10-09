d8651153dbb0467c17847fd6af9f4b2464cb6fad
HuSTLeR1709 <nabhjeetkatiyar3@gmail.com>
2023-10-10 01:45:44 +0530

    Added

---
+++ script.js
@@ -0,0 +1,20 @@
+const v= document.querySelector('#value');
+
+const increment= () => {
+    let value = parseInt(v.innerText);
+    value=value+1;
+    v.innerText=value;
+
+}
+
+const decrement= () => {
+    let value = parseInt(v.innerText);
+    value=value-1;
+    v.innerText=value;
+}   
+
+const reset=()=>{
+    let value =parseInt(v.innerText);
+    value=0;
+    v.innerText=value;
+}
